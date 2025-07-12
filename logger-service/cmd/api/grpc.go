b94699781202abd63ce91bb5cdd4fc17402e6e79
rbalajee <rahrkb4@gmail.com>
2025-07-12 18:06:53 +0530

    setup gRPC listener

--- logger-service/cmd/api/grpc.go
+++ logger-service/cmd/api/grpc.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"log-service/data"
 	"log-service/logs"
+	"net"
+
+	"google.golang.org/grpc"
 )
 
 type LogServer struct {
@@ -27,4 +32,21 @@ func (l *LogServer) WriteLog(ctx context.Context, req *logs.LogRequest) (*logs.L
 
 	res := &logs.LogResponse{Result: "logged"}
 	return res, nil
+}
+
+func (app *Config) gRPCListen() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", gRpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen for gRPC: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	logs.RegisterLogServiceServer(s, &LogServer{Models: data.MongoLogRepository{}})
+
+	log.Printf("gRPC server started on port %s", gRpcPort)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to listen for gRPC: %v", err)
+	}
 }
--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -56,6 +56,8 @@ func main() {
 
 	go app.rpcListen()
 
+	go app.gRPCListen()
+
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
