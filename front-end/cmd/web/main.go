2cd5b7e07a56d24f4ce7c2c03378d4a9a8dbbb72
rbalajee <rahrkb4@gmail.com>
2025-07-17 22:42:35 +0530

    fixed k8s

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -6,7 +6,6 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
@@ -14,8 +13,8 @@ func main() {
 		render(w, "test.page.gohtml")
 	})
 
-	fmt.Println("Starting front end service on port 8081")
-	err := http.ListenAndServe(":8081", nil)
+	fmt.Println("Starting front end service on port 80")
+	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -47,7 +46,8 @@ func render(w http.ResponseWriter, t string) {
 		BrokerURL string
 	}
 
-	data.BrokerURL = os.Getenv("BROKER_URL")
+	//data.BrokerURL = os.Getenv("BROKER_URL")
+	data.BrokerURL = "http://localhost:8080"
 
 	if err := tmpl.Execute(w, data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
